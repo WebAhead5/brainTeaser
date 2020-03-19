7c227d5b08ae3fb026fccb58e0697a3e4c795903
fridabu <f0504070002@gmail.com>
2020-03-19 10:49:47 +0200

    f: adding logic of moving between questions

--- src/js/question.js
+++ src/js/question.js
@@ -1,6 +1,6 @@
 var questionsArray = questionsDb;
-const images_folder = "../../resource/img";
-const currentQuestion = 0;
+const images_folder = "http://localhost:63342/brainTeaser/resource/img/";
+var currentQuestion = 0;
 
 function showQuestion(index){
     //Question text
@@ -9,10 +9,37 @@ function showQuestion(index){
 
     //First row images
     firstRowImages = document.getElementById("images_table").rows[0].cells;
-    console.log(firstRowImages[0]);
-    console.log(images_folder + '/' + questionsArray[index].pictures[0]);
-    console.log(firstRowImages[0].getElementsByTagName('img'));
-    firstRowImages[0].getElementsByTagName('img').src = images_folder + questionsArray[index].pictures[0];
+    firstRowImages[0].getElementsByTagName('img')[0].src = images_folder + questionsArray[index].pictures[0];
+    firstRowImages[1].getElementsByTagName('img')[0].src = images_folder + questionsArray[index].pictures[1];
+
+    //Second row images
+    secondRowImages = document.getElementById("images_table").rows[1].cells;
+    secondRowImages[0].getElementsByTagName('img')[0].src = images_folder + questionsArray[index].pictures[2];
+    secondRowImages[1].getElementsByTagName('img')[0].src = images_folder + questionsArray[index].pictures[3];
 }
 
-showQuestion(0);
+showQuestion(currentQuestion);
+
+const imgClick = document.querySelector('#images_table');
+imgClick.addEventListener('click', (e) => {
+    console.log(e);
+  let result = -1;
+  if(e.path[2].rowIndex == 0){
+    if(e.path[1].cellIndex == 0)
+        result = 0;
+    else
+        result = 1;
+  }else if (e.path[2].rowIndex == 1){
+    if(e.path[1].cellIndex == 0)
+      result = 2;
+    else
+      result = 3;
+  }
+  if( result === questionsArray[currentQuestion].answer){
+    currentQuestion++;
+    showQuestion(currentQuestion);
+  }else{
+    alert("Wrong");
+  }
+
+});
